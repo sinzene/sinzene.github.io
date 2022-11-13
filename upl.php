ce4c7ce3ae40e1f247b0563be465ebb24544cd07
sinzene__ <85278030+sinzene@users.noreply.github.com>
2022-11-13 09:33:19 +0300

    upl

--- upl.php
+++ upl.php
@@ -11,3 +11,18 @@
         }
     }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>upload</title>
+</head>
+<body>
+    <form action="?" method="post" enctype="multipart/form-data">
+        <label>upload file</label>
+        <p><input type="file" name="file"></p>
+        <p><input type="submit" name="upload" value="Upload File"></p>
+    </form>
+</body>
+</html>

1de33281573eb0d35cec9ef0e3a82e5abdffaa79
sinzene__ <85278030+sinzene@users.noreply.github.com>
2022-11-13 09:16:13 +0300

    upl

---
+++ upload.php
@@ -0,0 +1,13 @@
+<?php
+    if (isset($_POST['upload'])) {
+        $file_name = $_FILES['file']['name'];
+        $file_type = $_FILES['file']['type'];
+        $file_size = $_FILES['file']['size'];
+        $file_tem_loc = $_FILES['file']['tmp_name'];
+        $file_store = "upload/".$file_name;
+
+        if (move_uploaded_file($file_tem_loc, $file_store)) {
+            echo "Files are Uploaded";
+        }
+    }
+?>
